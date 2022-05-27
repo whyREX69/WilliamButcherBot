0d8b580e5740a6ffc79f2f7d55e83e5b7f84b379
whyREX <86944364+whyREX69@users.noreply.github.com>
2022-05-27 22:39:59 +0600

    Update config.py

--- config.py
+++ config.py
@@ -29,15 +29,15 @@ if HEROKU:
 else:
    
 
-BOT_TOKEN=5189246830:AAEVwFNM_4jtpYSrAw0ntw8oFfXJy0kYNsY # Get it from @botfather
+BOT_TOKEN="5189246830:AAEVwFNM_4jtpYSrAw0ntw8oFfXJy0kYNsY" # Get it from @botfather
 
 API_ID=12388301
 
-API_HASH=f08f8caff5aac56229e9f02bd67e48ea
+API_HASH="f08f8caff5aac56229e9f02bd67e48ea"
 
 SESSION_STRING="BQC9B80AhNlO6PLO1JNuClzMy0h3w67CIWpCAA6o5SCHnBSk__pFcAiT5LpTj1fXy0boKOOBVt1qQTLLxtQ7J4PMcPykKX3LbAuZeB3aEW4nV4g4hpLsgO-zoEYtNTwV4ICw94nBxQiHauUlVjRVmXjnnaKFqD3DS0e-w1ZedxHnA59XGLK-XJV8dMhNc_uANV91iUIzq0JG0fQAqssEWzavvGVYRdw0D25Bvch9UvpNXjmOb66jltkYUpXVeW_vMF6pvIBRSlNTS9J53q3jEWdJgc2yujL6-qL6XWrpDu2RGSykJugEd95qHn5P6gAQbfbdn_NBvDSBOe2rW-vh70nZRkuwSQAAAAE0XDV5AA" # Check Readme to generate sessions
 
-USERBOT_PREFIX=/
+USERBOT_PREFIX="/"
 
 SUDO_USERS_ID=1943696216 # Sudo users have full access to everythin, don't trust anyone
 
@@ -49,11 +49,11 @@ MESSAGE_DUMP_CHAT=-1001706596784
 
 WELCOME_DELAY_KICK_SEC=300 # Edit if u want
 
-MONGO_URL=mongodb+srv://vcbot:vcbot@cluster0.dnn8y.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+MONGO_URL="mongodb+srv://vcbot:vcbot@cluster0.dnn8y.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 
-ARQ_API_URL=https://arq.hamker.in # Leave it like this
+ARQ_API_URL="https://arq.hamker.in" # Leave it like this
 
-ARQ_API_KEY=CENWFE-IQFMCB-VGTHJX-SNQRXR-ARQ # Get it from @ARQRobot
+ARQ_API_KEY="CENWFE-IQFMCB-VGTHJX-SNQRXR-ARQ" # Get it from @ARQRobot
 
 LOG_MENTIONS=0 # Make it 1 if you want to enable it
 
