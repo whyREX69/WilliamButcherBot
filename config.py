4f74ea7b787736b2d6eefe00645692b5db4bdab6
whyREX <86944364+whyREX69@users.noreply.github.com>
2022-05-27 23:03:51 +0600

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ HEROKU = bool(
 
 if HEROKU:
 
-    BOT_TOKEN = environ.get("BOT_TOKEN", 5189246830:AAEVwFNM_4jtpYSrAw0ntw8oFfXJy0kYNsY)
+    BOT_TOKEN = environ.get("BOT_TOKEN", "5189246830:AAEVwFNM_4jtpYSrAw0ntw8oFfXJy0kYNsY")
     API_ID = int(environ.get("API_ID", 12388301))
     API_HASH = environ.get("API_HASH", "f08f8caff5aac56229e9f02bd67e48ea")
     SESSION_STRING = environ.get("SESSION_STRING", "BQC9B80AhNlO6PLO1JNuClzMy0h3w67CIWpCAA6o5SCHnBSk__pFcAiT5LpTj1fXy0boKOOBVt1qQTLLxtQ7J4PMcPykKX3LbAuZeB3aEW4nV4g4hpLsgO-zoEYtNTwV4ICw94nBxQiHauUlVjRVmXjnnaKFqD3DS0e-w1ZedxHnA59XGLK-XJV8dMhNc_uANV91iUIzq0JG0fQAqssEWzavvGVYRdw0D25Bvch9UvpNXjmOb66jltkYUpXVeW_vMF6pvIBRSlNTS9J53q3jEWdJgc2yujL6-qL6XWrpDu2RGSykJugEd95qHn5P6gAQbfbdn_NBvDSBOe2rW-vh70nZRkuwSQAAAAE0XDV5AA")
@@ -20,8 +20,8 @@ if HEROKU:
     GBAN_LOG_GROUP_ID = int(environ.get("GBAN_LOG_GROUP_ID", -1001706596784))
     MESSAGE_DUMP_CHAT = int(environ.get("MESSAGE_DUMP_CHAT", -1001706596784))
     WELCOME_DELAY_KICK_SEC = int(environ.get("WELCOME_DELAY_KICK_SEC", 300))
-    MONGO_URL = environ.get("MONGO_URL", mongodb+srv://vcbot:vcbot@cluster0.dnn8y.mongodb.net/myFirstDatabase?retryWrites=true&w=majority)
-    ARQ_API_URL = environ.get("ARQ_API_URL", https://arq.hamker.in)
+    MONGO_URL = environ.get("MONGO_URL", "mongodb+srv://vcbot:vcbot@cluster0.dnn8y.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+    ARQ_API_URL = environ.get("ARQ_API_URL", "://arq.hamker.in")
     ARQ_API_KEY = environ.get("ARQ_API_KEY", CENWFE-IQFMCB-VGTHJX-SNQRXR-ARQ)
     LOG_MENTIONS = bool(int(environ.get("LOG_MENTIONS", 1)))
     RSS_DELAY = int(environ.get("RSS_DELAY", 300))
